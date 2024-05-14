6f743850dd804af5824b0b8fca09beccec5ca152
Natasha <nbosman9403@gmail.com>
2024-05-14 14:44:42 +1000

    jwt authentication and database change

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,88 @@
-import { Injectable} from "@nestjs/common";
+import { ForbiddenException, Injectable} from "@nestjs/common";
 import { User, Bookmark } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService, TokenExpiredError } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService{
-    constructor(private prisma: PrismaService){}
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService ){}
 
-    signup(){
-        return {msg: "I have signed up"};
+    async signup(dto: AuthDto){
+        // generate password hash
+        const hash = await argon.hash(dto.password)
+        //save the new user in db
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            });
+            delete user.hash;
+            return this.signToken(user.id, user.email);   
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){
+                // duplicate field, unique id violated
+                if(error.code === 'P2002'){
+                    throw new ForbiddenException(
+                        'Credentials Already in Use'
+                    )
+                }
+            }
+            throw error;
+        }
     }
 
-    signin(){
-        return {msg: "I am signing in"};
+     async signin(dto: AuthDto){
+
+        // find user  by email
+        // if the user does not exist throw exception
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        })
+
+        if(!user){
+            throw new ForbiddenException(
+                'Credentials incorrect',
+            )
+        }
+
+        // compare password
+        //if password is incorrect, throw an exception
+        const pwMatches = await argon.verify(user.hash, dto.password);
+        if (!pwMatches){
+            throw new ForbiddenException(
+                'Password incorrect'
+            )
+        }
+        delete user.hash;
+        return this.signToken(user.id, user.email);
+    }
+
+     async signToken(userId: number, email: string): Promise<{access_token: string}>{
+        const payload = {
+            sub: userId,
+            email
+        }
+
+        const secret = this.config.get('JWT_SECRET')
+
+        
+        const token = await this.jwt.signAsync(
+            payload, 
+            {
+            expiresIn: '15m',
+            secret: secret,
+        })
+        return {
+            access_token: token,
+        }
     }
 }
 
